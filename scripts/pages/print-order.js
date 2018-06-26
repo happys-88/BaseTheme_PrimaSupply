780ff1310e78dc4a2b934f04425799ab2b9d8e23
Happy <hmasih@stegtechnologies.com>
2018-06-26 14:06:58 +0530

    Print Page Setup

--- scripts/pages/print-order.js
+++ scripts/pages/print-order.js
@@ -13,7 +13,13 @@ function ($, _, Hypr, Backbone, HyprLiveContext, api) {
 	    var orderId = location.hash.substring(1);
     	api.request("GET", '/api/commerce/orders/'+orderId).then(function(body){ 
 			$('#print-order').empty().append(Hypr.getTemplate('modules/my-account/print-order').render({ model: body }));      
-		}); 
+		});
+
+		// Print the content of order history 
+        $(document).on('click','#printInvoice', function(){  
+         	window.print();   
+        });
+
 	}); 
 });  
  
