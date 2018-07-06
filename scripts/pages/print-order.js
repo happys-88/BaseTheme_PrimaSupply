da3770ba35ea4c0947ec45f9b7499d47d73ed32e
Happy <hmasih@stegtechnologies.com>
2018-07-06 14:18:00 +0530

    Print Invoice Changes

--- scripts/pages/print-order.js
+++ scripts/pages/print-order.js
@@ -11,35 +11,36 @@ function ($, _, Hypr, Backbone, HyprLiveContext, api) {
 	$(document).ready(function(){ 
 	    var orderId = location.hash.substring(1);
     	api.request("GET", '/api/commerce/orders/'+orderId).then(function(body){ 
-			var impact=0;
-			var m=0;
+    		var impact = 0; 
+			var m = 0;
 			var discountarray;
 			var couponwithimpact=[];
-			 var couponwithCode=[];
+			var couponwithCode=[];
+			
 			$.each(body.couponCodes, function(i, item) {
 				$.each(body.items, function(j, disc) {	
 					$.each(disc.productDiscounts, function(k, item) {
 						if(k>0){
 							if(item.couponCode == body.couponCodes[i]){
-								impact=impact+item.impact;
-								couponwithimpact[m]=item.couponCode;
+								impact = impact+item.impact;
+								couponwithimpact[m] = item.couponCode;
 								couponwithCode[m] = impact;
 								}
 						}else{
 							if(item.couponCode == body.couponCodes[i]){
-								impact=impact+item.impact;
-								couponwithimpact[m]=item.couponCode;
-								couponwithCode[m]=impact;								
+								impact = impact+item.impact;
+								couponwithimpact[m] = item.couponCode;
+								couponwithCode[m] = impact;								
 							}
 						}
 					 });
 				});
-				m=m+1;
+				m = m+1;
 			});
-			var comp1=_.compact(couponwithimpact);
-			var comp2=_.compact(couponwithCode);
-			discountarray=_.object(comp1,comp2);
-			var print = Backbone.MozuModel.extend({});
+			var comp1 =_.compact(couponwithimpact);
+			var comp2 =_.compact(couponwithCode);
+			discountarray =_.object(comp1,comp2);
+			var print = Backbone.MozuModel.extend({}); 
 			var printmodel = new print(body);
 			printmodel.set({
 			 	"discount": discountarray
