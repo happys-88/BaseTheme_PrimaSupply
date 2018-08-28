ac302df1dfe8afc1b46bcc12478a03c3f3d5119d
singh-vsc <vsingh@stegtechnologies.com>
2018-08-28 15:06:43 +0530

    mailchimp code

--- scripts/modules/confirmation.js
+++ scripts/modules/confirmation.js
@@ -68,7 +68,9 @@ require(["modules/jquery-mozu", "underscore", 'modules/api', "hyprlive", "module
                 var email = $('#emailId').val();
                 if(deals !== '') {
                     api.request("POST", "/mailchimp", {'accountId':email, 'deals':deals}).then(function (response){
-                       // console.log("Response 22 : "+JSON.stringify(response)); 
+                       // console.log("Response 22 : "+JSON.stringify(response));
+                       $('#PSNewsLetter').attr('checked', false);
+                       $('#PSBlogs').attr('checked', false);
                        $(".mz-look-email").show().delay(5000).fadeOut();
                     }, function(err) {
                         console.log("Failure : "+JSON.stringify(err));
