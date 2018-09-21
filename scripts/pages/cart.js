fb27c90e888d68960fa33f0645230387f0ee965e
Happy <hmasih@stegtechnologies.com>
2018-09-21 19:26:38 +0530

    Fixed PRM-173 and coupon issue on cart

--- scripts/pages/cart.js
+++ scripts/pages/cart.js
@@ -452,11 +452,10 @@ define([
             });
              
         },
-        removeCoupon: function() {
+        removeCoupon: function(e) {  
             var self = this;
-            var getCouponCode = this.$el.find('.mz-coupon-detail .mz-coupon-code').attr('id');
+            var getCouponCode = e.currentTarget.id; 
 
-            var apiData = require.mozuData('apicontext');  
             blockUiLoader.globalLoader();
             var serviceurl = '/api/commerce/carts/' + this.model.get('id') + '/coupons/' + getCouponCode;
             api.request('DELETE', serviceurl).then(function(response) {
