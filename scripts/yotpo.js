cea473986cfb675e718ae301b668f68494b99870
Ravina <rrani@stegtechnologies.com>
2019-01-03 17:02:07 +0530

    Fixed PRM-301

--- scripts/yotpo.js
+++ scripts/yotpo.js
@@ -168,9 +168,8 @@ function ($, _, Hypr, Backbone, HyprLiveContext, api, blockUiLoader) {
             var currentProduct = $(this);
             var shippingMessage = $(currentProduct).find(".shipping-messages");
             var prodCode = $(this).find(".mz-productlisting").data("mz-product");
-            var prod = findElement(productData, prodCode);
+            var prod = YotPo.findElement(productData, prodCode);
             var button = $(this).find('[data-mz-productcode="'+prodCode+'"]');
-            
             if (prod.fieldDisplayOOSProp && prod.fieldDisplayOOSPropVal.value == '4') {
                 $(this).find(".stock-message").html(itemDiscontinued);
             } else {
@@ -217,6 +216,12 @@ function ($, _, Hypr, Backbone, HyprLiveContext, api, blockUiLoader) {
                 button.attr("data-target", "#addonModal");
             }
         });
+    },
+    findElement: function(arr, element) {
+        var product = arr.filter(function(el) {
+          return el.productCode == element;
+        })[0];
+        return product;
     },
     showYotpoRatingStars: function(listClassName){
         if((listClassName === ".mz-productlist-item") || (listClassName === ".mz-product-quick-view")){  
@@ -273,11 +278,3 @@ function ($, _, Hypr, Backbone, HyprLiveContext, api, blockUiLoader) {
   return YotPo;
 
 });
-
-function findElement(arr, element) {
-    var product = arr.find(function(el) {
-      return el.productCode == element;
-    });
-    return product;
-}
-
